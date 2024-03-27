6dd1baf0f8ef3410e2bb0506e5d5094549f38711
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-03-28 01:16:01 +0900

    2주차 실습

--- week2/prac/back/routes/users.js
+++ week2/prac/back/routes/users.js
@@ -1,9 +1,80 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+	const id = req.query.id;
+	const id_to_name = {
+		1: "Alice",
+		2: "Bob",
+		3: "Charlie",
+	};
+	if (id === undefined) {
+		res.send("Please provide an id");
+	} else if (id in id_to_name) {
+		res.status(200).send(id_to_name[id]);
+	} else {
+		res.status(404).send("User not found");
+	}
+});
+
+router.post("/", function (req, res, next) {
+	const id = req.body.id;
+	const id_to_name = {
+		1: "Alice",
+		2: "Bob",
+		3: "Charlie",
+	};
+	if (id === undefined) {
+		res.send("Please provide an id");
+	} else if (id in id_to_name) {
+		res.status(200).send(id_to_name[id]);
+	} else {
+		res.status(404).send("User not found");
+	}
+});
+
+router.get("/:id", function (req, res, next) {
+	const id = req.params.id;
+	const name = req.query.name;
+	const id_to_name = {
+		1: "Alice",
+		2: "Bob",
+		3: "Charlie",
+	};
+	if (id === undefined) {
+		res.send("Please provide an id");
+	} else if (id in id_to_name) {
+		if (id_to_name[id] === name) {
+			res.status(200).json({ id: id, name: name });
+		} else {
+			res.status(401).send("Name does not match, access denied");
+		}
+	} else {
+		res.status(404).send("User not found");
+	}
+});
+
+router.post("/:id", function (req, res, next) {
+	const id = req.params.id;
+	const name = req.body.name;
+	const id_to_name = {
+		1: "Alice",
+		2: "Bob",
+		3: "Charlie",
+	};
+	if (id === undefined) {
+		res.status(299).send("Please provide an id");
+	} else if (id in id_to_name) {
+		if (id_to_name[id] === name) {
+			res.status(200).json({ id: id, name: name });
+		} else {
+			res.status(401).send("Name does not match, access denied");
+		}
+	} else {
+		res.status(404).send("User not found");
+	}
 });
 
 module.exports = router;
+
--- week2/prac/front/src/App.js
+++ week2/prac/front/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [num, setNum] = useState(0);
+	const [name, setName] = useState("");
+	const [isMatch, setIsMatch] = useState(false);
+
+	const sendID = () => {
+		axios
+			.get(`/api/users?id=${num}`)
+			.then((res) => {
+				setName(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+				setName("Error");
+			});
+	};
+
+	const matchIDName = () => {
+		axios
+			.post(`/api/users/${num}`, { name: name, number: num })
+			.then((res) => {
+				if (res.status === 299) {
+					setIsMatch(false);
+				} else {
+					setIsMatch(true);
+					console.log(res.data.id, res.data.name);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				setIsMatch(false);
+			});
+	};
+
+	useEffect(() => {
+		matchIDName();
+	}, [num, name]);
+
+	return (
+		<div className="App">
+			<div>{num}</div>
+			<div>{name}</div>
+			{isMatch ? <div>match!</div> : <div>not match!</div>}
+			<button
+				onClick={() => {
+					setNum(num + 1);
+				}}>
+				Click me!
+			</button>
+			<button onClick={sendID}>Send ID!</button>
+		</div>
+	);
 }
 
 export default App;
+

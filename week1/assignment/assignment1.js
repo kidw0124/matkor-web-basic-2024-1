b2ab7be0df1d70240014865aa055fbf50914ba5d
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-03-27 15:53:59 +0900

    과제1-3

--- week1/assignment/assignment1.js
+++ week1/assignment/assignment1.js
@@ -3,23 +3,39 @@ let num = prompt("몇번 치실래요?");
 num = Number(num);
 // num = parseInt(num);
 
-const cnt_dom = document.getElementById("cnt");
-const egg_dom = document.getElementById("egg");
+const cnt1_dom = document.getElementById("cnt1");
+const egg1_dom = document.getElementById("egg1");
+const cnt2_dom = document.getElementById("cnt2");
+const egg2_dom = document.getElementById("egg2");
 
-cnt_dom.innerHTML = num;
+cnt1_dom.innerHTML = num;
+cnt2_dom.innerHTML = num;
 
 const org_num = num;
+let num1 = num;
+let num2 = num;
+
 const color = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
-const hitEgg = () => {
-	num--;
-	cnt_dom.innerHTML = num;
-	cnt_dom.style.color = color[Math.floor(((org_num - num) / org_num) * 7)];
-	if (num == 0) {
+const hitEgg1 = () => {
+	num1--;
+	cnt1_dom.innerHTML = num1;
+	cnt1_dom.style.color = color[Math.floor(((org_num - num1) / org_num) * 7)];
+	if (num1 == 0) {
 		alert("성공!");
 	}
 };
 
-egg_dom.addEventListener("click", hitEgg);
+const hitEgg2 = () => {
+	num2--;
+	cnt2_dom.innerHTML = num2;
+	cnt2_dom.style.color = color[Math.floor(((org_num - num2) / org_num) * 7)];
+	if (num2 == 0) {
+		alert("성공!");
+	}
+};
+
+egg1_dom.addEventListener("click", hitEgg1);
+egg2_dom.addEventListener("click", hitEgg2);
 
 // for (let i = 0; i < org_num; i++) {
 // 	egg_dom.click();

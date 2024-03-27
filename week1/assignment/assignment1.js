5c183926b9a14a529877adcdb5ee3686c730c0e5
kidw0124 <38641848+kidw0124@users.noreply.github.com>
2024-03-27 15:30:58 +0900

    과제 1-2

--- week1/assignment/assignment1.js
+++ week1/assignment/assignment1.js
@@ -9,9 +9,11 @@ const egg_dom = document.getElementById("egg");
 cnt_dom.innerHTML = num;
 
 const org_num = num;
+const color = ["red", "orange", "yellow", "green", "blue", "navy", "purple"];
 const hitEgg = () => {
 	num--;
 	cnt_dom.innerHTML = num;
+	cnt_dom.style.color = color[Math.floor(((org_num - num) / org_num) * 7)];
 	if (num == 0) {
 		alert("성공!");
 	}
@@ -19,7 +21,7 @@ const hitEgg = () => {
 
 egg_dom.addEventListener("click", hitEgg);
 
-for (let i = 0; i < org_num; i++) {
-	egg_dom.click();
-	console.log(i);
-}
+// for (let i = 0; i < org_num; i++) {
+// 	egg_dom.click();
+// 	console.log(i);
+// }
